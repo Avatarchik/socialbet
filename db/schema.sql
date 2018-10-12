06a25b67d3a3c8702289b2d2b5c7bab68f3bfb42
taswartz <taswartz@umich.edu>
2018-10-12 13:34:12 -0400

    Fixed data type issues, and syntax errors in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,8 @@
 # Social Bet Database
-CREATE DATABASE socialbetdb;
 USE socialbetdb;
 
 CREATE TABLE users (
-	uid INT NOT NULL,
+	uid INTEGER NOT NULL,
 	firstname VARCHAR(20) NOT NULL,
 	lastname VARCHAR(20) NOT NULL,
 	birthdate VARCHAR(20) NOT NULL,
@@ -12,22 +11,23 @@ CREATE TABLE users (
 );
 
 CREATE TABLE friends ( 
-	user1id INT NOT NULL,
-	user2id INT,
-	PRIMARY KEY(user1,user2),
-	FOREIGN KEY(user1id) REFERENCES users(uid)
+	user1id INTEGER NOT NULL,
+	user2id INTEGER,
+	PRIMARY KEY(user1id,user2id),
+	FOREIGN KEY(user1id) REFERENCES users(uid),
+	FOREIGN KEY(user2id) REFERENCES users(uid)
 );
 
 CREATE TABLE bets (
-	bid INT NOT NULL,
-	cid INT NOT NULL,
-	value  NOT NULL, 
-	user1id INT NOT NULL,
-	user2id INT,
+	bid INTEGER NOT NULL,
+	cid INTEGER NOT NULL,
+	value FLOAT NOT NULL, 
+	user1id INTEGER NOT NULL,
+	user2id INTEGER,
 	sport VARCHAR(50) NOT NULL,
 	timeplaced DATE NOT NULL,
 	timeaccepted DATE,
-	PRIMARY KEY(obid),
+	PRIMARY KEY(bid),
 	FOREIGN KEY(user1id) REFERENCES users(uid),
 	FOREIGN KEY(user2id) REFERENCES users(uid)
 );
