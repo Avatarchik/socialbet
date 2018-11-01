3114f42e9abf48f328a12cdcf6e977e189f95cc0
John Krieg <johnkrieg@Johns-Mac.local>
2018-10-31 18:10:41 -0700

    added faux profile page

---
+++ frontend/SocialBet/SocialBet/Profile.swift
@@ -0,0 +1,34 @@
+//
+//  Profile.swift
+//  SocialBet
+//
+//  Created by John Krieg on 10/31/18.
+//  Copyright © 2018 Nick Cargill. All rights reserved.
+//
+
+import UIKit
+
+class Profile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var ProfilePicture: UIImageView!
+    
+    @IBAction func Test(_ sender: Any) {
+        performSegue(withIdentifier: "gotologinfromprofile", sender: self)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
