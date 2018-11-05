160d76494f34744a4a7df6ccac54757f5ce151ca
John Krieg <johnkrieg@Johns-Mac.local>
2018-11-05 09:36:21 -0800

    try committing again

--- frontend/SocialBet/SocialBet/ViewController.swift
+++ frontend/SocialBet/SocialBet/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        username = "";
+        pwhash = "";
     }
 
 
-    @IBOutlet weak var LoginUsername: UITextField!
-    @IBOutlet weak var LoginPassword: UITextField!
+    @IBOutlet weak var _username: UITextField!
+    @IBOutlet weak var _password: UITextField!
     
     
     @IBAction func LoginSubmit(_ sender: Any) {
@@ -29,7 +31,34 @@ class ViewController: UIViewController {
     
     
     @IBAction func GoToRegistration(_ sender: Any) {
-        performSegue(withIdentifier: "LoginToRegistration", sender: self)
+        // take the sha256 hash of the password
+        username = _username.text!;
+        pwhash = "TODO";
+        
+        
+        // Submit a POST request containing the username and hash(passowrd)
+        let url = URL(string: domain)
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.httpBody = (username + "&" + pwhash).data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            
+            guard let data = data, error == nil, else {
+                print("error=\(error)")
+                return
+            }
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        //performSegue(withIdentifier: "LoginToRegistration", sender: self)
     }
 }
 
