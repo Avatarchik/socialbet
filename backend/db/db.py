2d2a999ab80618673b275fb9d4b87327e14f0adb
isvidler <isvidler@umich.edu>
2018-11-05 20:08:14 -0500

    iusjdgfoidfkgl

--- backend/db/db.py
+++ backend/db/db.py
@@ -4,7 +4,6 @@ import pymysql
 
 
 def get_users(data):
-	#TODO
 
 	# Open database connection
 	db_config = get_db_config()
@@ -14,17 +13,58 @@ def get_users(data):
 	cursor = db.cursor()
 
 	# execute SQL query using execute() method.
-	# TODO:
 	cursor.execute("SELECT * FROM users")
 
 	# Fetch a single row using fetchone() method.
 	data = cursor.fetchone()
-	print ("Database version : %s " % data)
 
 	# disconnect from server
 	db.close()
-	pass
 
+	return data
+
+def get_user(data):
+
+	user_id = data['user_id']
+
+	# Open database connection
+	db_config = get_db_config()
+	db = pymysql.connect(db_config['host'], db_config['username'], db_config['password'], db_config['database_name'])
+
+	# prepare a cursor object using cursor() method
+	cursor = db.cursor()
+
+	# execute SQL query using execute() method.
+	cursor.execute("SELECT * FROM users WHERE uid=\"" + user_id + "\"")
+
+	# Fetch a single row using fetchone() method.
+	data = cursor.fetchone()
+
+	# disconnect from server
+	db.close()
+
+	return data
+
+def get_bets(direct, live):
+
+
+	# Open database connection
+	db_config = get_db_config()
+	db = pymysql.connect(db_config['host'], db_config['username'], db_config['password'], db_config['database_name'])
+
+	# prepare a cursor object using cursor() method
+	cursor = db.cursor()
+
+	# execute SQL query using execute() method.
+	cursor.execute('SELECT * FROM bets WHERE direct="' + direct + '" and live="' + live+ '"')
+
+	# Fetch a single row using fetchone() method.
+	data = cursor.fetchone()
+
+	# disconnect from server
+	db.close()
+
+	return data
 
 # functions for user information
 def create_user(data):
