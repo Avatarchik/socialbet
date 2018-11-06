722817b0fbc4727b378c5a8c60f195582648b1c3
Chris Terech <christerech@0587392953.wireless.umich.net>
2018-11-05 19:44:21 -0500

    get_users

--- backend/db/db.py
+++ backend/db/db.py
@@ -15,7 +15,7 @@ def get_users(data):
 
 	# execute SQL query using execute() method.
 	# TODO:
-	cursor.execute("SELECT VERSION()")
+	cursor.execute("SELECT * FROM users")
 
 	# Fetch a single row using fetchone() method.
 	data = cursor.fetchone()
@@ -25,10 +25,36 @@ def get_users(data):
 	db.close()
 	pass
 
+
+# functions for user information
 def create_user(data):
+	#TODO
+
+	# Open database connection
+	db_config = get_db_config()
+	db = pymysql.connect(db_config['host'], db_config['username'], db_config['password'], db_config['database_name'])
+
+	# prepare a cursor object using cursor() method
+	cursor = db.cursor()
+
+	
+
+	pass
+
+def drop_user(data):
 	#TODO
 	pass
 
+# functions for bet information
 def place_bet(data):
 	#TODO
 	pass
+
+# functions for friends information
+def get_friends(data):
+	#TODO
+	pass
+
+
+
+
