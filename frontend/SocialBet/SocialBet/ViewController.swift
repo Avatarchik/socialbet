f4de90d329c35285a335ebdb67d3eb678679bf02
John Krieg <jpkrieg@umich.edu>
2018-11-07 00:11:05 -0800

    Networking Implemented (not working completely yet), Configuration File added

--- frontend/SocialBet/SocialBet/ViewController.swift
+++ frontend/SocialBet/SocialBet/ViewController.swift
@@ -9,19 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+    
+    // PAGE DATA ///////////////////////////////////////////////////////////////////////////////////
+    
     @IBOutlet weak var LoginUsername: UITextField!
     @IBOutlet weak var LoginPassword: UITextField!
     
+    // PAGE MANAGEMENT METHODS /////////////////////////////////////////////////////////////////////
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // reset the credentials
+        LoginUsername.text = ""
+        LoginPassword.text = ""
+    }
+
+    // PAGE INTERACTION METHODS ////////////////////////////////////////////////////////////////////
+    
+    // Login Button
+    // Send a POST request to /api/v1/accounts/login/
+    // {username:"", pwhash:""}
     @IBAction func LoginSubmit(_ sender: Any) {
-        //TODO - Send LoginUsername and LoginPassword for confirmation
+        // get the user credentials from the textboxes
+        let submitusername = LoginUsername.text
+        let submitpassword = LoginPassword.text
+        
+        // create a dictionary to pass the parameters in
+        let parameters = ["username": submitusername, "password": submitpassword] as! Dictionary<String, String>
+
+        // create and send a POST request
+        let response = sendPOST(uri: "/api/v1/accounts/login/", parameters: parameters)
+        
+        // alert the user of success/failure, and either navigate away or refresh the page
+        if response.error != nil {
+            performSegue(withIdentifier: "LoginToRegistration", sender: self)
+        }
+        else{
+            alert(title: "Authentication Error", message: "The provided username and password pair wasn't recognized. Try again.")
+            viewDidLoad()
+        }
     }
     
     @IBAction func LoginHelp(_ sender: Any) {
@@ -30,6 +57,17 @@ class ViewController: UIViewController {
     
     @IBAction func GoToRegistration(_ sender: Any) {
         performSegue(withIdentifier: "LoginToRegistration", sender: self)
+    }
+    
+    // HELPER METHODS //////////////////////////////////////////////////////////////////////////////
+    
+    // the following are global functions
+    func alert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in alert.dismiss(animated: true, completion: nil)}))
+        
+        self.present (alert, animated: true, completion: nil)
     }
 }
 
---
+++ frontend/SocialBet/SocialBet/config.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  SocialBet
+//
+//  Created by John Krieg on 11/4/18.
+//  Copyright © 2018 Nick Cargill. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CommonCrypto
+
+let domain = "socialbet.jpkrieg.com"
+var username = "default"
+var pwhash = "default"
