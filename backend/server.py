86c1aa1b36cfc54d8b131d623614540528b6208d
isvidler <isvidler@umich.edu>
2018-11-07 13:27:21 -0500

    soidfsdf

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,11 @@
 from flask import Flask
 import os
 
+
+app = Flask(__name__)
+
 # Create flask app
 def create_app(test_config=None):
-	app = Flask(__name__)
 	# create and configure the app
 	app = Flask(__name__, instance_relative_config=True)
 	app.config.from_mapping(
