20528adf1ccb9e6d119a957505eee8d5f86434a6
Chris Terech <christerech@Chriss-MacBook-Pro.local>
2018-11-08 01:01:09 -0500

    fixed bugs in api

--- backend/api/games.py
+++ backend/api/games.py
@@ -4,7 +4,7 @@ from .api_utils import create_http_response
 app = Flask(__name__)
 games = Blueprint('games', __name__)
 
-@app.route('/api/games/get_games/')
+@games.route('/api/games/get_games/')
 def get_games():
 	# TODO
 
