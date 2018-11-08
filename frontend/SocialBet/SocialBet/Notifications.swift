32286b923f812d2b50ddb1e236a7107b7c534a9d
Alex Chapp <chapp@umich.edu>
2018-11-08 04:18:36 -0500

    WIP...just added the skeleton...need to add logic

---
+++ frontend/SocialBet/SocialBet/Notifications.swift
@@ -0,0 +1,61 @@
+//
+//  Notifications.swift
+//  SocialBet
+//
+//  Created by Alex Chapp on 11/8/18.
+//  Copyright © 2018 Nick Cargill. All rights reserved.
+//
+
+import UIKit
+
+class Notifications: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func Accepted(_ sender: Any) {
+        
+    }
+    
+    @IBAction func Declined(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func Requests(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func Results(_ sender: Any) {
+        
+    }
+    
+    
+    @IBOutlet weak var NotificationFeed: UICollectionView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
