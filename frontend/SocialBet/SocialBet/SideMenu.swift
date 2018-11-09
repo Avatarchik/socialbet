9864859c15a6348d4223d05f80ae95a0c91e3eb3
nickcargill <cargilln@umich.edu>
2018-11-09 02:14:37 -0500

    Button functionality, need to pull settings page

--- frontend/SocialBet/SocialBet/SideMenu.swift
+++ frontend/SocialBet/SocialBet/SideMenu.swift
@@ -12,6 +12,29 @@ class SideMenu: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
+        switch indexPath.row {
+        case 0: print("Add search bar functionality")
+        case 1: toProfile()
+        case 2: toNotifications()
+        case 3: toSettings()
+        default: break
+        }
     }
-
+    
+    
+    @IBAction func toProfile() {
+        performSegue(withIdentifier: "ToProfile", sender: self)
+    }
+    
+    @IBAction func toNotifications() {
+        performSegue(withIdentifier: "ToNotifications", sender: self)
+    }
+    
+    @IBAction func toSettings() {
+        
+    }
+    
+    
+    
+    
 }
