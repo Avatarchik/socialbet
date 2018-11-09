65517a2a6a8cf049eced9dde848e77467adb5db3
Chris Terech <christerech@Chriss-MacBook-Pro.local>
2018-11-08 21:13:48 -0500

    profanity

--- backend/db/data.sql
+++ backend/db/data.sql
@@ -39,7 +39,7 @@ VALUES
 (12, 'Denver Broncos', 'Los Angeles Chargers', '1-7', '2-6', '11/18/2018 4:05PM'); 
 
 # BETS
-INSERT INTO bets (bet_id, game_id, time_placed, game_time, num_comments, message, ammount, user1, user2, direct, accepted)
+INSERT INTO bets (bet_id, game_id, time_placed, game_time, num_comments, message, ammount, user1, user2, team1, team2, direct, accepted)
 VALUES
 (1,1,'11/16/2018 3:52PM', '11/18/2018 1:00PM', 0, 'Nick is going down!', 50.0, 'achapp', 'cterech', 'Detroit Lions', 'Chicago Bears', 1, 1), 
 (2,3,'11/16/2018 4:19PM', '11/18/2018 1:00PM', 0, 'Ittai is going down!', 100.0, 'achapp', 'jkrieg','Miami Dolphins', 'New Orleans Saints', 1, 1);
