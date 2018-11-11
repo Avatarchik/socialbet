6561426a1391e58a8e1cb1fa26f93ef21bbb82b7
Ryan Shelly <rshelly@umich.edu>
2018-11-11 16:44:22 -0500

    writing users.py

--- backend/api/users.py
+++ backend/api/users.py
@@ -6,9 +6,37 @@ app = Flask(__name__)
 users = Blueprint('users', __name__)
 
 
+@users.route('/api/users/exist')
+def check_user():
+	log_user = request.args.get('loguser')
+	auth = request.args.get('auth')
+	auth_ = db.authenticate(log_user, auth)
+	if not auth_:
+		result = {}
+		result['errors'] = []
+		result['errors'].append('unauthenticated user')
+		return create_http_response(data=result, errors=result['errors'])
+
+	is_friend = request.args.get('friends')
+	if is_friend:
+		data = {}
+		data['user_name'] = log_user
+		all_friends = db.get_friends(data)
+
+	else:
+		
+
+
 @users.route('/api/users/list')
 def list_users():
-	# TODO
+	log_user = request.args.get('loguser')
+	auth = request.args.get('auth')
+	auth_ = db.authenticate(log_user, auth)
+	if not auth_:
+		result = {}
+		result['errors'] = []
+		result['errors'].append('unauthenticated user')
+		return create_http_response(data=result, errors=result['errors'])
 
 	user = db.get_users()
 
@@ -16,14 +44,28 @@ def list_users():
 
 @users.route('/api/users/find')
 def get_user():
-
+	log_user = request.args.get('loguser')
+	auth = request.args.get('auth')
+	auth_ = db.authenticate(log_user, auth)
+	if not auth_:
+		result = {}
+		result['errors'] = []
+		result['errors'].append('unauthenticated user')
+		return create_http_response(data=result, errors=result['errors'])
 	# TODO
 	return create_http_response()
 
 
 @users.route('/api/users/create')
 def create_user():
-
+	log_user = request.args.get('loguser')
+	auth = request.args.get('auth')
+	auth_ = db.authenticate(log_user, auth)
+	if not auth_:
+		result = {}
+		result['errors'] = []
+		result['errors'].append('unauthenticated user')
+		return create_http_response(data=result, errors=result['errors'])
 	#TODO
 
 	return create_http_response()
