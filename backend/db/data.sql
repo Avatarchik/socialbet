65a798790e35263eba728ddae1925b62eb55d450
Chris Terech <christerech@0587394107.wireless.umich.net>
2018-11-12 18:09:26 -0500

    added functionality for pics in db

--- backend/db/data.sql
+++ backend/db/data.sql
@@ -1,15 +1,15 @@
 USE socialbetdb
 
 # USERS
-INSERT INTO users (user_name, first_name, last_name, phone, auth)
+INSERT INTO users (user_name, first_name, last_name, phone, profile_pic_url auth)
 VALUES
-('cterech', 'Chris','Terech','734-417-6331','HIEOS3753H83FHFKS'), 
-('rshelly','Ryan','Shelly','949-290-6670','SDJEI3752E7EFH381J4G9'),
-('ncargill','Nick','Cargill','914-479-2054','H3829HVVUELAW8572'),
-('isvidler','Ittai','Svilder','912-409-0127', '382HF89EHN29E9321AA'),
-('achapp','Alex','Chapp','734-498-0573','3UF920927201F0OSPEU33'),
-('jkrieg','John','Krieg','734-201-9423','29720JSWO37GHEBMXNW'),
-('taswartz','Trevor','Swartz','914-479-2054','19VUE3HS0HEJ37F35PW0');
+('cterech', 'Chris','Terech','734-417-6331', '/root/socialbet/backend/user_profile_pics/terech.png', 'HIEOS3753H83FHFKS'), 
+('rshelly','Ryan','Shelly','949-290-6670', '/root/socialbet/backend/user_profile_pics/shelly.png', 'SDJEI3752E7EFH381J4G9'),
+('ncargill','Nick','Cargill','914-479-2054', '/root/socialbet/backend/user_profile_pics/cargill.png', 'H3829HVVUELAW8572'),
+('isvidler','Ittai','Svilder','912-409-0127', '/root/socialbet/backend/user_profile_pics/svidler.png', '382HF89EHN29E9321AA'),
+('achapp','Alex','Chapp','734-498-0573', '/root/socialbet/backend/user_profile_pics/chapp.png', '3UF920927201F0OSPEU33'),
+('jkrieg','John','Krieg','734-201-9423', '/root/socialbet/backend/user_profile_pics/krieg.png', '29720JSWO37GHEBMXNW'),
+('taswartz','Trevor','Swartz','914-479-2054', '/root/socialbet/backend/user_profile_pics/swartz.png', '19VUE3HS0HEJ37F35PW0');
 
 # FRIENDS
 INSERT INTO friends (user1,user2)
@@ -25,18 +25,16 @@ VALUES
 # GAMES
 INSERT INTO games(game_id, team1, team2, team1_url, team2_url, record1, record2, game_time)
 VALUES
-(1, 'Detroit Lions', 'Chicago Bears', '1-7', '2-6', '11/18/2018 1:00PM'),
-(2, 'New England Patriots', 'Philadelphia Eagles','1-7', '2-6', '11/18/2018 1:00PM'), 
-(3, 'Miami Dolphins', 'New Orleans Saints','1-7', '2-6', '11/18/2018 1:00PM'),
-(4, 'Pittsburgh Steelers', 'New York Giants','1-7', '2-6', '11/18/2018 3:00PM'),
-(5, 'Atlanta Falcons', 'Houston Texans', '1-7', '2-6', '11/18/2018 3:00PM'),
-(6, 'Detroit Lions', 'Chicago Bears', '1-7', '2-6','11/22/2018 12:30PM'),
-(7, 'New York Giants', 'Philadelphia Eagles', '1-7', '2-6', '11/25/2018 1:00PM'),
-(8, 'Seattle Seahawks', 'Carolina Panthers', '1-7', '2-6', '11/25/2018 1:00PM'),
-(9, 'Oakland Raiders', 'Baltimore Ravens', '1-7', '2-6', '11/25/2018 1:00PM'),
-(10, 'Green Bay Packers', 'Minnesota Vikings', '1-7', '2-6', '11/25/2018 8:20PM'),
-(11, 'Cincinnati Bengals', 'Baltimore Ravens', '1-7', '2-6', '11/18/2018 1:00PM'),
-(12, 'Denver Broncos', 'Los Angeles Chargers', '1-7', '2-6', '11/18/2018 4:05PM'); 
+(1, 'Detroit Lions', 'Chicago Bears', '/root/socialbet/backend/team_logos/lions.png', '/root/socialbet/backend/team_logos/bears.png', '1-7', '2-6', '11/18/2018 1:00PM'),
+(2, 'New England Patriots', 'Philadelphia Eagles', '/root/socialbet/backend/team_logos/patriots.png', '/root/socialbet/backend/team_logos/eagles.png', '1-7', '2-6', '11/18/2018 1:00PM'), 
+(3, 'Miami Dolphins', 'New Orleans Saints', '/root/socialbet/backend/team_logos/dolphins.png', '/root/socialbet/backend/team_logos/saints.png', '1-7', '2-6', '11/18/2018 1:00PM'),
+(4, 'Pittsburgh Steelers', 'New York Giants', '/root/socialbet/backend/team_logos/steelers.png', '/root/socialbet/backend/team_logos/giants.png', '1-7', '2-6', '11/18/2018 3:00PM'),
+(5, 'Atlanta Falcons', 'Houston Texans', '/root/socialbet/backend/team_logos/falcons.png', '/root/socialbet/backend/team_logos/texans.png', '1-7', '2-6', '11/18/2018 3:00PM'),
+(6, 'Seattle Seahawks', 'Carolina Panthers', '/root/socialbet/backend/team_logos/seahawks.png', '/root/socialbet/backend/team_logos/panthers.png','1-7', '2-6', '11/25/2018 1:00PM'),
+(7, 'Oakland Raiders', 'Baltimore Ravens', '/root/socialbet/backend/team_logos/raiders.png', '/root/socialbet/backend/team_logos/ravens.png', '1-7', '2-6', '11/25/2018 1:00PM'),
+(8, 'Green Bay Packers', 'Minnesota Vikings', '/root/socialbet/backend/team_logos/packers.png', '/root/socialbet/backend/team_logos/vikings.png', '1-7', '2-6', '11/25/2018 8:20PM'),
+(9, 'Cincinnati Bengals', 'Miami Dolphins', '/root/socialbet/backend/team_logos/bengals.png', '/root/socialbet/backend/team_logos/dolphins.png', '1-7', '2-6', '11/18/2018 1:00PM'),
+(10, 'Denver Broncos', 'Los Angeles Chargers', '/root/socialbet/backend/team_logos/broncos.png', '/root/socialbet/backend/team_logos/charges.png', '1-7', '2-6', '11/18/2018 4:05PM'); 
 
 # BETS
 INSERT INTO bets (bet_id, game_id, time_placed, game_time, num_comments, message, ammount, user1, user2, team1, team2, direct, accepted)
