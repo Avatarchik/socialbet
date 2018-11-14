4665f4cefc89e333598c8e792470da3c0b7bdd22
isvidler <isvidler@umich.edu>
2018-11-14 15:32:15 -0500

    direct pending bets endpoint

--- backend/api/feeds.py
+++ backend/api/feeds.py
@@ -141,12 +141,45 @@ def list_closed_bets_bet_history():
 
 @feeds.route('/api/feeds/direct_bets_pending')
 def list_direct_bets_pending():
+
+    # Authenticate user
     loguser = request.args.get('loguser')
     auth = request.args.get('auth')
     if not db.authenticate(loguser, auth):
         return create_http_response(errors=['unauthenticated user'])
 
-    # TODO
+    # Get live bets and construct JSON
+    db_bets = db.get_pending_direct_bets(loguser)
 
-    pass
+    bets = []
+    for db_bet in db_bets:
+        bet = db_bet
+
+        db_user1 = db.get_user(db_bet['user1'])
+        user1 = {
+            'user_id': db_user1['user_name'],
+            'first_name': db_user1['first_name'],
+            'last_name': db_user1['last_name'],
+            'profile_pic_url': db_user1['profile_pic_url'],
+            'team': db_bet['team1']
+        }
+        db_user2 = db.get_user(db_bet['user2'])
+        user2 = {
+            'user_id': db_user2['user_name'],
+            'first_name': db_user2['first_name'],
+            'last_name': db_user2['last_name'],
+            'profile_pic_url': db_user2['profile_pic_url'],
+            'team': db_bet['team2']
+        }
+
+        bet['user1'] = user1
+        bet['user2'] = user2
+        bets.append(bet)
+
+    # Return JSON response
+    result = {
+        'bets': bets
+    }
+
+    return create_http_response(data=result)
 
