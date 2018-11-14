2d8a2961abbc291340eba92f7f501f53fa23757e
isvidler <isvidler@umich.edu>
2018-11-14 12:31:03 -0500

    updated users and database user schema to hold more chars in auth

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
 	first_name VARCHAR(20) NOT NULL,
 	last_name VARCHAR(20) NOT NULL,
 	phone VARCHAR(12),
-  	auth VARCHAR(70) NOT NULL,
+  	auth VARCHAR(100) NOT NULL,
  	profile_pic_url VARCHAR(1000),
 	PRIMARY KEY(user_name)
 );
