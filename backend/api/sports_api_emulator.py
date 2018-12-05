ddce56c13d6b40f6353fd055b4621c588a8ef22d
Chris Terech <christerech@Chriss-MacBook-Pro-2.local>
2018-12-04 22:02:51 -0500

    sports API done

--- backend/api/sports_api_emulator.py
+++ backend/api/sports_api_emulator.py
@@ -13,29 +13,29 @@ games = Blueprint('sports_api_emulator', __name__)
 def get_games_and_scores():
 
 	log_user = request.args.get('loguser')
-    auth_token = request.args.get('auth')
-    authenticated = db.authenticate(log_user, auth_token)
-    if not authenticated:
-       return create_http_response(errors=['unauthenticated user'])
+	auth_token = request.args.get('auth')
+	authenticated = db.authenticate(log_user, auth_token)
+	if not authenticated:
+	   return create_http_response(errors=['unauthenticated user'])
 
-    games = db.get_games()
-    response = []
-    for g in games:
-    	game = {}
-    	game['game_id'] = g[0]
-    	game['team1'] = g[1]
-    	game['team2'] = g[2]
-    	game['team1_url'] = g[3]
-    	game['team2_url'] = g[4]
-    	game['record1'] = g[5]
-    	game['record2'] = g[6]
-    	game['game_time'] = g[7]
-    	game['homeScore'] = None
-    	game['awayScore'] = None
-    	game['eventStartsAt'] = None
-    	game['League'] = None
-    	game['AwayTeam'] = None
-    	game['HomeTeam'] = None
-    	response.append(game)
+	games = db.get_games()
+	response = []
+	for g in games:
+		game = {}
+		game['game_id'] = g[0]
+		game['team1'] = g[1]
+		game['team2'] = g[2]
+		game['team1_url'] = g[3]
+		game['team2_url'] = g[4]
+		game['record1'] = g[5]
+		game['record2'] = g[6]
+		game['game_time'] = g[7]
+		game['homeScore'] = None
+		game['awayScore'] = None
+		game['eventStartsAt'] = None
+		game['League'] = None
+		game['AwayTeam'] = None
+		game['HomeTeam'] = None
+		response.append(game)
 
-    return jsonify(response), 200,  {'ContentType': 'application/json'}
+	return jsonify(response), 200,  {'ContentType': 'application/json'}
