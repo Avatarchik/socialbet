bc364755b42ccd56d7405664393fdd2ec641224c
nickcargill <cargilln@umich.edu>
2018-12-05 16:50:39 -0500

    Debugging notification end point

--- backend/api/games.py
+++ backend/api/games.py
@@ -65,14 +65,37 @@ def get_unnotified_games():
     if not authenticated:
         return create_http_response(errors=['unauthenticated user'])
 
-    games_to_notify = db.unnotified_bets(log_user)
-    print("Right after the SQL query")
-    print(games_to_notify)
+
+    bets_to_notify = db.unnotified_bets(log_user)
     response = []
 
-    #for game in games_to_notify:
-        #response.append(game)
+    for db_bet in bets_to_notify:
+        bet = db_bet
+        db_user1 = db.get_user(db_bet['user1'])
+        user1 = {
+            'username': db_user1['user_name'],
+            'first_name': db_user1['first_name'],
+            'last_name': db_user1['last_name'],
+            'profile_pic_url': db_user1['profile_pic_url'],
+            'team': db_bet['team1']
+        }
+        db_user2 = db.get_user(db_bet['user2'])
+        user2 = {
+            'username': db_user2['user_name'],
+            'first_name': db_user2['first_name'],
+            'last_name': db_user2['last_name'],
+            'profile_pic_url': db_user2['profile_pic_url'],
+            'team': db_bet['team2']
+        }
 
-    #print(response)
+        bet['user1'] = user1
+        bet['user2'] = user2
+        response.append(bet)
+        db.set_bet_to_notified(bet['bet_id'])
 
-    return #create_http_response(data=response)
+    # Return JSON response
+    result = {
+        'bets': response
+    }
+
+    return create_http_response(data=response)
