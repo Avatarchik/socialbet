ad7a200da9ecc453bf6b362da3cde4e84020044f
John Krieg <jpkrieg@umich.edu>
2018-12-05 18:36:04 +0000

    added accept bet methos for contranct client and test

--- backend/api/ethereum/test_ethereum_client.py
+++ backend/api/ethereum/test_ethereum_client.py
@@ -9,5 +9,16 @@ def create_bet_test():
     amount = 0.2
     ethereum_client.create_bet(bet_id, amount, test_account)
 
+def accept_bet_test():
+    w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+    test_account = w3.eth.accounts[0]
+    bet_id = 1
+    amount = 0.2
+    ethereum_client.accept_bet(bet_id, amount, test_account)
+
+
 if __name__ == "__main__":
-    create_bet_test()
+    if sys.argv[1] == 'create_bet':
+        create_bet_test()
+    elif sys.argv[1] == 'accept_bet':
+        accept_bet_test()
