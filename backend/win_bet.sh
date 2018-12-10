4356d94c13c14222f6da9b117e41cca106154806
John Krieg <jpkrieg@umich.edu>
2018-12-10 17:44:08 +0000

    win_bet and pending bet for chapp

--- backend/win_bet.sh
+++ backend/win_bet.sh
@@ -1,10 +1,10 @@
 url="http://165.227.180.80:5000"
 
 
-bet_id=24
-team1_score=30
-team2_score=49
-winner="isvidler"
+bet_id=1
+team1_score=49
+team2_score=30
+winner="achapp"
 curl -X POST -H "Content-Type: application/json" $url/api/betting/win_bet/ \
     -d "{\"bet_id\": \"$bet_id\", \"team1_score\": \"$team1_score\", \"team2_score\": \"$team2_score\", \"winner\": \"$winner\"}"
 
